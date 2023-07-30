05e461317cd1458aad8d4c44bedf3e3a51eece07
zzingobomi <zzingo5@gmail.com>
2023-07-30 17:11:22 +0900

    feat: blockchain

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/zzingobomi/zzingocoin/blockchain"
+)
 
 func main() {
-	fmt.Println("Welcome to zzingocoin")
+	chain := blockchain.GetBlockchain()
+	chain.AddBlock("Second Block")
+	chain.AddBlock("Third Block")
+	chain.AddBlock("Fourth Block")
+	for _, block := range chain.AllBlocks() {
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %s\n", block.Hash)
+		fmt.Printf("Prev Hash: %s\n", block.PrevHash)
+	}
 }
